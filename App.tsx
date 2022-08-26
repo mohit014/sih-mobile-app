0c5c31d1a7cc49ae823820d068892ff8e1c1dc20
mohit014 <mohit038@icloud.com>
2022-08-26 17:50:57 +0530

    app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, View } from "react-native";
+
+import * as React from "react";
+import { WebView } from "react-native-webview";
+import {
+  SafeAreaProvider,
+  SafeAreaView,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <StatusBar />
+      <MainApp />
+    </SafeAreaProvider>
   );
 }
+
+const MainApp = () => {
+  const inset = useSafeAreaInsets();
+  return (
+    <WebView
+      style={[styles.container, { marginTop: inset.top }]}
+      source={{ uri: "https://habitrack.vercel.app/app" }}
+    />
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
